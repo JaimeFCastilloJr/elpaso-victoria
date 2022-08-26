4b0dd454753dfc263c35ac838e68f1ab426d28ca
Jaime Castillo <jaimefcastillojr@gmail.com>
2022-08-26 13:42:19 -0500

    Decreased transition of buttons on 'Menu' tab

--- assets/js/book.js
+++ assets/js/book.js
@@ -17,8 +17,8 @@ let maxLocation = numOfPapers + 1;
 
 function openBook() {
     book.style.transform = "translateX(50%)";
-    prevBtn.style.transform = "translateX(-180px)";
-    nextBtn.style.transform = "translateX(180px)";
+    prevBtn.style.transform = "translateX(-75px)";
+    nextBtn.style.transform = "translateX(75px)";
 }
 
 function closeBook(isAtBeginning) {
